cf31cd62c8ab997ac61a7094b7d46ffa6e759c40
Lingatsu <115959500+Lingatsu@users.noreply.github.com>
2024-05-04 19:59:13 +0200

    initial commit

---
+++ src/fr/declaration/variable/DeclarationApp.java
@@ -0,0 +1,19 @@
+package fr.declaration.variable;
+
+public class DeclarationApp {
+    public static void main(String[] args) {
+        byte a = 1;
+        short b = 2;
+        int c = 3;
+        long d = 4;
+        float e = 5.0f;
+        double f = 6.0;
+        boolean g = true;
+        char h = 'a';
+        String i = "hello word";
+        System.out.println(f);
+
+        String randomString = "Voici le résultat d’un calcul :\n 1+5=6";
+        System.out.println(randomString);
+    }
+}
